3f6824b77f71cc3bf1d2f6dfdea04b697a21bd1e
morshedul-antor <antor.morshedul@gmail.com>
2023-12-04 13:26:17 +0600

    feat: hash function add

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,11 @@
 const User = require("../models/userModel");
 const userRepo = require("../repositories/userRepo");
+const bcrypt = require("bcrypt");
 
 const createUser = async (data) => {
+  const hashedPassword = await bcrypt.hash(data.password, 10);
+  data.password = hashedPassword;
+
   const newUser = new User(data);
   return await newUser.save();
 };
@@ -10,7 +14,17 @@ const getAllUsers = async (skip, limit) => {
   return await userRepo.getAllUsers(skip, limit);
 };
 
+const getUserByPhone = async (phone) => {
+  return await userRepo.findOne(phone);
+};
+
+const getUserByEmail = async (email) => {
+  return await userRepo.findOne(email);
+};
+
 module.exports = {
   createUser,
   getAllUsers,
+  getUserByPhone,
+  getUserByEmail,
 };
