142f3551ac32d2efca7b73a943ec719e1afcf04d
morshedul-antor <antor.morshedul@gmail.com>
2023-12-04 12:44:01 +0600

    add: dotenv

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = require("./app");
+dotenv.config();
 
 mongoose
-  .connect("mongodb://localhost:27017/auth")
+  .connect(process.env.DB_URL)
   .then(() => console.log("DB connection successfull..."))
   .catch((err) => console.log(err));
 
