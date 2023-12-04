64626575649a105390f2e36b143f027f062cd45b
morshedul-antor <antor-morshedul@gmail.com>
2023-12-04 23:11:23 +0600

    feat: global error handler add

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+// error handler
+const errorExceptions = require("./errors/exceptions");
+app.use(errorExceptions);
+
 // root api
 app.get("/", (req, res) => {
   res.send({ message: "Nodejs Authentication!" });
 });
 
-// router
+// api router
 const routerV1 = require("./routes/router");
 app.use("/api/v1", routerV1);
 
