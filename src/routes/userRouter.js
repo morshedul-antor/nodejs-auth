c1bcfea01c31fbba2ea972cdea69e0d42ebd8d96
morshedul-antor <antor.morshedul@gmail.com>
2023-12-04 17:17:41 +0600

    feat: secure api enpoints

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,24 +2,34 @@
 const express = require("express");
 const router = express.Router();
 
+const verifyToken = require("../middlewares/verifyToken");
 const userService = require("../services/userService");
 
-router.post("/signup", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    const newUser = await userService.createUser(req.body);
-    res.json(newUser);
+    const data = await userService.createUser(req.body);
+    res.json(data);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
-router.get("/all", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const skip = parseInt(req.query.skip) || 0;
     const limit = parseInt(req.query.limit) || 10;
 
-    const newUsers = await userService.getAllUsers(skip, limit);
-    res.json(newUsers);
+    const data = await userService.getAllUsers(skip, limit);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.get("/:id", verifyToken, async (req, res) => {
+  try {
+    const data = await userService.getUserById(req.params.id);
+    res.json(data);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
