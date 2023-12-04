7366c245a3cbdce064ec751fa1e1bfbf810f98e7
morshedul-antor <antor.morshedul@gmail.com>
2023-12-04 17:15:50 +0600

    feat: query by id add

--- src/repositories/userRepo.js
+++ src/repositories/userRepo.js
@@ -8,14 +8,22 @@ const getUserByEmail = async (email) => {
   return await User.findOne({ email });
 };
 
+const getUserById = async (id) => {
+  return await User.findById(id, { __v: 0, password: 0 });
+};
+
 const getAllUsers = async (skip, limit) => {
   const dataAll = await User.find();
-  const data = await User.find({}, { __v: 0 }).skip(skip).limit(limit);
+  const data = await User.find({}, { __v: 0, password: 0 })
+    .skip(skip)
+    .limit(limit);
+
   return [{ result: dataAll.length }, data];
 };
 
 module.exports = {
   getUserByPhone,
   getUserByEmail,
+  getUserById,
   getAllUsers,
 };
