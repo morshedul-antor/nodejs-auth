56020bede8db7632de8ad2368c65b01e3af5ae6f
morshedul-antor <antor.morshedul@gmail.com>
2023-12-04 17:16:15 +0600

    feat: login method add

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,7 @@
 const User = require("../models/userModel");
 const userRepo = require("../repositories/userRepo");
+
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 const createUser = async (data) => {
@@ -10,21 +12,34 @@ const createUser = async (data) => {
   return await newUser.save();
 };
 
+const login = async (data) => {
+  const user = await userRepo.getUserByPhone(data.identifier);
+  if (!user) {
+    throw new Error("Invalid username or password!");
+  }
+
+  const isMatch = await bcrypt.compare(data.password, user.password);
+  if (!isMatch) {
+    throw new Error("Invalid username or password!");
+  }
+
+  const token = jwt.sign({ sid: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "48h",
+  });
+  return { token: token, type: "bearer" };
+};
+
 const getAllUsers = async (skip, limit) => {
   return await userRepo.getAllUsers(skip, limit);
 };
 
-const getUserByPhone = async (phone) => {
-  return await userRepo.findOne(phone);
-};
-
-const getUserByEmail = async (email) => {
-  return await userRepo.findOne(email);
+const getUserById = async (id) => {
+  return await userRepo.getUserById(id);
 };
 
 module.exports = {
   createUser,
   getAllUsers,
-  getUserByPhone,
-  getUserByEmail,
+  getUserById,
+  login,
 };
