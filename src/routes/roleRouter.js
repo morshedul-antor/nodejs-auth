431b296e13b831074013216053731bdf3ed877e0
morshedul-antor <antor.morshedul@gmail.com>
2023-12-11 11:24:12 +0600

    feat: api endpoint add

--- src/routes/roleRouter.js
+++ src/routes/roleRouter.js
@@ -22,4 +22,31 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const data = await roleService.getById(req.params.id);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const data = await roleService.updateById(req.params.id, req.body);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const data = await roleService.get();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 module.exports = router;
