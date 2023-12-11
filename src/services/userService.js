2fb1e38e34157e922691b7571dac7a2ca0d41405
morshedul-antor <antor.morshedul@gmail.com>
2023-12-11 13:23:58 +0600

    add: active status check

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,17 @@
 const BaseService = require("./baseService");
 const userRepo = require("../repositories/userRepo");
+const roleRepo = require("../repositories/roleRepo");
 
 const { createHash, validateHash } = require("../utils/security");
 const { createToken } = require("../utils/token");
 
 class UserService extends BaseService {
   async createUser(data) {
+    const role = await roleRepo.getByKey({ name: data.role });
+    if (!role) {
+      throw new Error("Role not found!");
+    }
+
     const userPhone = await userRepo.getUserByPhone(data.phone);
     if (userPhone) {
       throw new Error("Phone number already exist!");
@@ -18,6 +24,7 @@ class UserService extends BaseService {
 
     const hashedPassword = await createHash(data.password);
     data.password = hashedPassword;
+    data.roleId = role._id;
 
     const newUser = await super.create(data);
     const { password, ...user } = newUser.toObject();
@@ -28,6 +35,10 @@ class UserService extends BaseService {
     const user = await userRepo.getUserByIdentifier(data.identifier);
     if (!user) {
       throw new Error("Invalid username or password!");
+    }
+
+    if (!user.active) {
+      throw new Error("You're currently not an active user!");
     }
 
     const isMatch = await validateHash(data.password, user.password);
